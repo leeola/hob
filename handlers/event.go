ddd52f8c8af9ed2f42274a07353b484cb767fb27
Lee Olayvar <leeolayvar@gmail.com>
2016-11-17 17:19:36 -0800

    feat: tweaked log messages

--- handlers/event.go
+++ handlers/event.go
@@ -11,18 +11,19 @@ import (
 func EventHandler(events hob.Events, actions hob.Actions) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log := middleware.GetLog(r)
+		log.Debug("event requested", "url", r.URL)
 
 		eventName := chi.URLParam(r, "event")
 		actionName, ok := events[eventName]
 		if !ok {
-			log.Info("unknown event requested", "eventName", eventName)
+			log.Debug("unknown event requested", "eventName", eventName)
 			http.Error(w, http.StatusText(404), 404)
 			return
 		}
 
 		action, ok := actions[actionName]
 		if !ok {
-			log.Info("unknown action configured", "actionName", actionName)
+			log.Warn("unknown action configured", "actionName", actionName)
 			http.Error(w, http.StatusText(404), 404)
 			return
 		}
